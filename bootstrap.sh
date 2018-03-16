214421c9413b8583a98b48253d40a82809b2744a
Timo V <devel.timo.v@gmail.com>
2018-03-16 01:03:47 +0100

    Intermediate commit for ssh support

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,7 @@ a2dissite 000-default
 a2ensite phabricator
 
 # Configure php
-sed -e 's|^[; ]*always_populate_raw_post_data.*?$|always_populate_raw_post_data = -1|' \
+sed -e 's|^[; ]*always_populate_raw_post_data.*$|always_populate_raw_post_data = -1|' \
     -e 's|^[; ]*post_max_size.*$|post_max_size = 32M|' \
     -e 's|^[; ]*date.timezone.*$|date.timezone = Europe/Berlin|' \
     -i /etc/php5/apache2/php.ini
@@ -36,6 +36,20 @@ su phab -c "git clone https://github.com/phacility/phabricator.git && cd phabric
 
 # Configure git
 cp /opt/phabricator/resources/sshd/sshd_config.phabricator.example /etc/ssh/sshd_config
+sed -e 's|^AuthorizedKeysCommand .*$|AuthorizedKeysCommand /opt/sshd/phabricator-ssh-hook.sh|' \
+    -e 's|vcs-user|git|' \
+    -e 's|^Port.*$|Port 22|' \
+    -i /etc/ssh/sshd_config
+
+mkdir -p /opt/sshd
+cp /opt/phabricator/resources/sshd/phabricator-ssh-hook.sh /opt/sshd
+chmod 755 /opt/sshd/phabricator-ssh-hook.sh
+sed -e 's|^VCSUSER=.*$|VCSUSER="git"|' \
+    -e 's|^ROOT=.*$|ROOT="/opt/phabricator"|' \
+    -i /opt/sshd/phabricator-ssh-hook.sh
+
+mkdir -p /var/run/sshd
+
 
 
 
